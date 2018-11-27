f37363eacd94351d0b2822a02249cd84f7590576
Liu <sliu125@illinois.edu>
2018-11-27 16:15:49 -0600

    Fixed the bugs in GestureCollector and Fixed the Player OVR

--- Assets/Scripts/GestureCollector.cs
+++ Assets/Scripts/GestureCollector.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 
 public class GestureCollector : MonoBehaviour {
 
@@ -11,7 +12,7 @@ public class GestureCollector : MonoBehaviour {
     private List<float> x = new List<float>();
     private List<float> y = new List<float>();
     private List<float> z = new List<float>();
-    private List<NamedGesture> data = new List<NamedGesture>
+    private List<NamedGesture> data = new List<NamedGesture>();
     private bool collecting = false;
     private Vector3 gestureStartingPosition;
 
@@ -20,34 +21,39 @@ public class GestureCollector : MonoBehaviour {
     }
 
     void Update() {
-
-        if (Input.GetButtonDown("RightSide")) {
+        
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0) { // Right, Side-button
             Collect();
         } else {
             collecting = false;
         }
         
-        if (Input.GetButtonDown("X")) {
+        if (OVRInput.GetDown(OVRInput.Button.One)) { // Right, A
+            Debug.Log("Labeled True");
             LabelTrue();
         }
-        if (Input.GetButtonDown("Y")) {
+        if (OVRInput.GetDown(OVRInput.Button.Two)) { // Right, B
+            Debug.Log("Labeled False");
             LabelFalse();
         }
-        if (Input.GetButtonDown("Z")) {
+        if (OVRInput.GetDown(OVRInput.Button.Three)) { // Left, X
+            Debug.Log("Saving");
             Save();
         }
     }
 
     void Collect() {
         if (!collecting) {
+            Debug.Log("Started collecting");
             collecting = true;
             gestureStartingPosition = wand.position;
             x.Clear();
             y.Clear();
+            z.Clear();
         }
-        x.Add(wand.position.x - gestureStartingPosition.x)
-        y.Add(wand.position.y - gestureStartingPosition.y)
-        z.Add(wand.position.z - gestureStartingPosition.z)
+        x.Add(wand.position.x - gestureStartingPosition.x);
+        y.Add(wand.position.y - gestureStartingPosition.y);
+        z.Add(wand.position.z - gestureStartingPosition.z);
     }
 
     void LabelTrue() {
@@ -62,7 +68,7 @@ public class GestureCollector : MonoBehaviour {
     void LabelFalse() {
         NamedGesture ng = new NamedGesture();
         ng.name = "!";
-        ns.name += label;
+        ng.name += label;
         ng.gesture = new List<List<float>>();
         ng.gesture.Add(x);
         ng.gesture.Add(y);
@@ -70,22 +76,23 @@ public class GestureCollector : MonoBehaviour {
         data.Add(ng);
     }
     void Save() {
+
         string saveString1 = "";
         string saveString2 = "";
         for(int i = 0; i < data.Count; i++){
             string name = data[i].name;
-            if (name[0] == "!") {
+            if (name[0] == '!') {
                 if(saveString2.Length != 0) saveString2 += "\n";
-                saveString2 += JsonUtility.ToJson(data[i]);
+                saveString2 += NamedGestureToJson(data[i]);
             } else {
                 if(saveString1.Length != 0) saveString1 += "\n";
-                saveString1 += JsonUtility.ToJson(data[i]);
+                saveString1 += NamedGestureToJson(data[i]);
             }
         }
-        string path1 = "Assets/Output/"
+        string path1 = "Assets/Output/";
         path1 += label;
         path1 += ".ndjson";
-        string path2 = "Assets/Output/"
+        string path2 = "Assets/Output/";
         path2 += "!";
         path2 += label;
         path2 += ".ndjson";
@@ -95,6 +102,26 @@ public class GestureCollector : MonoBehaviour {
         StreamWriter writer2 = new StreamWriter(path2, true);
         writer2.Write(saveString2);
         writer2.Close();
+        Debug.Log("Finished Saving");
+    }
+
+    string NamedGestureToJson(NamedGesture ng) {
+        string s = "{\"name\":\"";
+        s += ng.name;
+        s += "\",\"gesture\":[";
+        string gs = ""; // gesture string
+        foreach (List<float> fl in ng.gesture) {
+            if (gs.Length != 0) gs += ",";
+            string fs = "["; // float list string
+            foreach (float f in fl) {
+                if (fs.Length != 1) fs += ",";
+                fs += f.ToString("F4");
+            }
+            gs += fs + "]";
+        }
+        s += gs;
+        s += "]}";
+        return s;
     }
 }
 
